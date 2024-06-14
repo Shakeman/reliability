34cc35c4bfe7c69116d0fe8f64f25b2f723fe6a6
John Doe <johndoe@example.com>
2024-06-14 13:11:32 -0700

    Increased test coverage of Reliability_Testing

--- tests/test_Reliability_testing.py
+++ tests/test_Reliability_testing.py
@@ -13,6 +13,7 @@ from reliability.Reliability_testing import (
     reliability_test_duration,
     reliability_test_planner,
     sample_size_no_failures,
+    sequential_sampling_chart,
     two_proportion_test,
 )
 
@@ -28,7 +29,10 @@ def test_reliability_one_sample_proportion():
 
 def test_two_proportion_test():
     results = two_proportion_test(
-        sample_1_trials=500, sample_1_successes=490, sample_2_trials=800, sample_2_successes=770,
+        sample_1_trials=500,
+        sample_1_successes=490,
+        sample_2_trials=800,
+        sample_2_successes=770,
     )
     assert_allclose(results[0], -0.0004972498915250083, rtol=rtol, atol=atol)
     assert_allclose(results[1], 0.03549724989152493, rtol=rtol, atol=atol)
@@ -46,10 +50,13 @@ def test_reliability_test_planner():
     assert_allclose(test_plan.number_of_failures, 7, rtol=rtol, atol=atol)
     assert_allclose(test_plan.test_duration, 19520, rtol=rtol, atol=atol)
 
-@pytest.mark.flaky(reruns=5)
+
 def test_reliability_test_duration():
     test_duration = reliability_test_duration(
-        MTBF_required=2500, MTBF_design=3000, consumer_risk=0.2, producer_risk=0.2,
+        MTBF_required=2500,
+        MTBF_design=3000,
+        consumer_risk=0.2,
+        producer_risk=0.2,
     )
     assert_allclose(test_duration, 231615.79491309822, rtol=rtol, atol=atol)
 
@@ -69,7 +76,7 @@ def test_chi2test():
         46226,
         np.inf,
     ]  # it is not necessary to specify the bins and leaving them unspecified is usually best
-    chisq = chi2test(distribution=dist, data=data, bins=bins, show_plot=False, print_results=False)
+    chisq = chi2test(distribution=dist, data=data, bins=bins, show_plot=True, print_results=True)
     assert_allclose(chisq.chisquared_critical_value, 12.591587243743977, rtol=rtol, atol=atol)
     assert_allclose(chisq.chisquared_statistic, 3.1294947845652, rtol=rtol, atol=atol)
     assert chisq.hypothesis == "ACCEPT"
@@ -78,19 +85,99 @@ def test_chi2test():
 def test_KStest():
     data = mileage().failures
     dist = Normal_Distribution(mu=30011, sigma=10472)
-    results = KStest(distribution=dist, data=data, print_results=False, show_plot=False)
+    results = KStest(distribution=dist, data=data, print_results=True, show_plot=True)
     assert_allclose(results.KS_critical_value, 0.13402791648569978, rtol=rtol, atol=atol)
     assert_allclose(results.KS_statistic, 0.07162465859560846, rtol=rtol, atol=atol)
     assert results.hypothesis == "ACCEPT"
 
 
+dist_types: list[str] = ["Weibull", "Gamma", "Loglogistic", "Normal", "Lognormal", "Gumbel"]
 
-def test_likelihood_plot():
-    old_design = [2,9,23,38,67,2,11,28,40,76,3,17,33,45,90,4,17,34,55,115,6,19,34,56,126,9,21,37,57,197]
-    new_design = [15,116,32,148,61,178,67,181,75,183]
 
-    fig = likelihood_plot(distribution="Weibull", failures=old_design, CI=[0.9, 0.95])
+@pytest.mark.parametrize("dist_type", dist_types)
+def test_likelihood_plot(dist_type: str):
+    old_design = [
+        2,
+        9,
+        23,
+        38,
+        67,
+        2,
+        11,
+        28,
+        40,
+        76,
+        3,
+        17,
+        33,
+        45,
+        90,
+        4,
+        17,
+        34,
+        55,
+        115,
+        6,
+        19,
+        34,
+        56,
+        126,
+        9,
+        21,
+        37,
+        57,
+        197,
+    ]
+    new_design = [15, 116, 32, 148, 61, 178, 67, 181, 75, 183]
+
+    fig = likelihood_plot(distribution=dist_type, failures=old_design, CI=[0.9, 0.95])
     assert isinstance(fig, plt.Figure)
 
-    fig = likelihood_plot(distribution="Weibull", failures=new_design, CI=[0.9, 0.95])
+    fig = likelihood_plot(distribution=dist_type, failures=new_design, CI=[0.9, 0.95])
     assert isinstance(fig, plt.Figure)
+
+
+def test_sequential_sampling():
+    test_results = [
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        1,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        1,
+        0,
+        0,
+        0,
+        0,
+        0,
+        1,
+    ]
+    results = sequential_sampling_chart(p1=0.01, p2=0.10, alpha=0.05, beta=0.10, test_results=test_results)
+    assert results.shape[0] == 101
+    assert results.shape[1] == 3
